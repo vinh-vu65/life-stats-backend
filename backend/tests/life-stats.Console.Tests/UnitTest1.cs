2b1063d5c6fd9b693349e84250b961911186ef29
Vinh Vu <vinh.vu@myob.com>
2024-01-02 18:44:55 +1100

    add dummy tests for CI

--- backend/tests/life-stats.Console.Tests/UnitTest1.cs
+++ backend/tests/life-stats.Console.Tests/UnitTest1.cs
@@ -2,9 +2,17 @@ namespace life_stats.Console.Tests;
 
 public class UnitTest1
 {
-    [Fact]
-    public void Test1()
-    {
+    private int Add(int x, int y) => x + y;
 
+    [Fact]
+    public void WhenGivenTwoNumbers_Add_ShouldReturnTheSumOfTheTwoNumbers()
+    {
+        Assert.Equal(4, Add(2, 2));
+    }
+
+    [Fact]
+    public void WhenGivenTwoNumbers_Add_ShouldNotReturnTheIncorrectSumOfTheTwoNumbers()
+    {
+        Assert.NotEqual(5, Add(2, 2));
     }
 }
